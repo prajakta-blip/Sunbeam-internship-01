6e7f4f82a8e8ea6e3b5bb1ef4b93d727078705a0
Prajakta Patil <pprajakta20062gmail.com>
2025-12-10 21:55:20 +0530

    add sub

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+def add(a, b):
+    return a + b
+
+result = add(num1, num2)
+print("Addition =", result)
--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-
+import arithmetic
 print("Hello, World!")
